4679b3fa378640578ab9eb9ed8ff220b66ea8de5
Spencer Skylar Chan <54919210+schance995@users.noreply.github.com>
2024-09-08 22:15:54 -0400

    Add license to quantum.py

--- src/vendored/quantum.py
+++ src/vendored/quantum.py
@@ -1,6 +1,22 @@
+# This file has been modified from the original:
+# https://github.com/XanaduAI/qml-benchmarks/blob/main/src/qml_benchmarks/models/dressed_quantum_circuit.py
+#
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pennylane as qml
 
-# TODO: insert Xanadu copyright notice above
 # TODO: the linearly separable version doesn't use entanglement and can be simulated by n * 1-qubit circuits.
 # careful - jax doesn't error on invalid indices
 # shape of weights should be (layers, n_qubits)
